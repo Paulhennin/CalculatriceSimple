ae08bb8ca7d36b1245c5454cb5772c8b1405894d
Paulhennin <henninpaul@gmail.com>
2021-07-09 22:34:08 +0200

    a little bit of css and responsive

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,14 +3,11 @@ import React from 'react';
 
 // == Import
 import Calculatrice from 'src/components/Calculatrice';
-import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
     <Calculatrice />
   </div>
 );
